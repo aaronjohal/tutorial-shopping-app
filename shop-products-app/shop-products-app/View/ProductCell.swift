85296ee708581c49c6cc0f160a1aee0be67acc85
Aaron Johal <aaronjohal@gmail.com>
2020-04-13 17:25:43 +0100

    a days work adding collection view UI elements

---
+++ shop-products-app/shop-products-app/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  shop-products-app
+//
+//  Created by Aaron Johal on 13/04/2020.
+//  Copyright © 2020 Aaron Johal. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productDesc: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
